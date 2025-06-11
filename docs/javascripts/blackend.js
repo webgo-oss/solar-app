4656e831820d0243a352047ba572c90a88839056
Hamza <webgo235@gmail.com>
2025-06-11 21:02:48 +0530

    some changes

--- docs/javascripts/blackend.js
+++ docs/javascripts/blackend.js
@@ -45,7 +45,7 @@ scene.add(ambientLight);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
-controls.enableRotation=false;
+controls.autoRotate=false;
 controls.enableZoom=false;
 function onScroll() {
   const scrollY = window.scrollY;
